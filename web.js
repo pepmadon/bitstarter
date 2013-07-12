5f08a1e8893622e6af9ec53bdff5128cbec55075
Pep Madon <PepMadon@gmail.com>
2013-07-11 21:54:24 -0500

    Updated web.js with fileread

--- web.js
+++ web.js
@@ -2,9 +2,22 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+
+var fs = require('fs');
+var indexBuffer = fs.readFileSync('index.html').toString();
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(indexBuffer);
 });
+
+
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
